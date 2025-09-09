346b81d4b783ae70f6048f71e52b9f1faa258894
suvanshenoy <suvanshenoy155@gmail.com>
2025-09-09 17:23:52 +0530

    chore: ignore some files and directories for docker, create docker-compose.dev.yml for development and add host, port in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true,
+		strictPort: true,
+		port: 3000,
+	},
+});
